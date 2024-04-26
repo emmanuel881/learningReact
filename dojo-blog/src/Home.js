71167024b6a5c7be440c5dafb5e8f44e21293db9
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-04-26 11:25:38 +0300

    outputting list

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,24 +1,30 @@
-import { useState, userState } from "react";
+import { useState } from "react";
 
 const Home = () => {
-  const [name, setName] = useState("mario");
-  //let name = "mario";
-  let counter = 0;
-  const handleClick = (e) => {
-    if (counter === 0) {
-      setName("Tom");
-      counter = 1;
-    } else {
-      setName("mario");
-      counter = 0;
-    }
-    console.log("clicked");
-  };
+  const [blogs, setBlog] = useState([
+    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
+    {
+      title: "Welcome party!",
+      body: "lorem ipsum...",
+      author: "yoshi",
+      id: 2,
+    },
+    {
+      title: "Web dev top tips",
+      body: "lorem ipsum...",
+      author: "mario",
+      id: 3,
+    },
+  ]);
+
   return (
     <div className="home">
-      <h2>Welcome</h2>
-      <p>{name}</p>
-      <button onClick={handleClick}>Click me</button>
+      {blogs.map((blog) => (
+        <div className="blog-preview" key={blog.id}>
+          <h2>{blog.title}</h2>
+          <p>written by {blog.author}</p>
+        </div>
+      ))}
     </div>
   );
 };
