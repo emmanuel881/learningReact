4ae02c7a2ad1d7a475e7306545540eea85612a56
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-04-27 12:29:00 +0300

    error handling

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -4,22 +4,32 @@ import BlogList from "./BlogList";
 const Home = () => {
   const [blogs, setBlog] = useState(null);
   const [isPending, setIsPending] = useState(true);
+  const [error, setError] = useState(null);
 
   //lets fetch data from a json server
   useEffect(() => {
     fetch("http://localhost:8000/blogs")
       .then((res) => {
+        if (!res.ok) {
+          throw Error("can't find the resource !");
+        }
         //returns a js object
         return res.json();
       })
       .then((data) => {
         setBlog(data);
         setIsPending(false);
+        setError(null);
+      })
+      .catch((err) => {
+        setError(err.message);
+        setIsPending(false);
       });
   }, []);
 
   return (
     <div className="home">
+      {error && <div>{error}</div>}
       {isPending && <div>Loading...</div>}
       {blogs && (
         <BlogList blogs={blogs} title="Blogs" />
