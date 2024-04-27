c386334106b15e96b59334bc93c9742da1bdac80
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-04-27 09:59:15 +0300

    my commt

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
@@ -22,6 +22,10 @@ const Home = () => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlog(newBlogs);
   };
+
+  useEffect(() => {
+    alert("deleted");
+  }, [blogs]);
 
   return (
     <div className="home">
