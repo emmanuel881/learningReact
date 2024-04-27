134c12216c11168bf41d32026b0be8a106518597
Emmanuel Saura Ogola <emmanuelsaura007@gmail.com>
2024-04-27 10:30:45 +0300

    JSON server db

--- dojo-blog/src/Home.js
+++ dojo-blog/src/Home.js
@@ -2,34 +2,31 @@ import { useState, useEffect } from "react";
 import BlogList from "./BlogList";
 
 const Home = () => {
-  const [blogs, setBlog] = useState([
-    { title: "My new website", body: "lorem ipsum...", author: "mario", id: 1 },
-    {
-      title: "Welcome party!",
-      body: "lorem ipsum...",
-      author: "yoshi",
-      id: 2,
-    },
-    {
-      title: "Web dev top tips",
-      body: "lorem ipsum...",
-      author: "mario",
-      id: 3,
-    },
-  ]);
+  const [blogs, setBlog] = useState(null);
 
   const handleDelete = (id) => {
     const newBlogs = blogs.filter((blog) => blog.id !== id);
     setBlog(newBlogs);
   };
 
+  //lets fetch data from a json server
   useEffect(() => {
-    alert("deleted");
-  }, [blogs]);
+    fetch("http://localhost:8000/blogs")
+      .then((res) => {
+        //returns a js object
+        return res.json();
+      })
+      .then((data) => {
+        setBlog(data);
+      });
+  }, []);
 
   return (
     <div className="home">
-      <BlogList blogs={blogs} handleDelete={handleDelete} title="Blogs" />
+      {blogs && (
+        <BlogList blogs={blogs} handleDelete={handleDelete} title="Blogs" />
+        //outputs blog only if has been fully loaded to prevent mapping a null value
+      )}
     </div>
   );
 };
